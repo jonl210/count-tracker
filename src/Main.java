2e8d92c57c9a3354e5b03644a666a16dfd9ac097
jonathan <jonathanlambert247@gmail.com>
2022-09-06 21:40:02 -0700

    new version includes true count, and editable deck totals

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,113 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class Main implements KeyListener {
-    int count = 0;
-    String windowTitle = "CountTracker v1.1.0";
+public class Main implements KeyListener, ActionListener {
+    int runningCount = 0;
+    int totalDecks  = 4;
+    int trueCount = 0;
+    String windowTitle = "CountTracker v2.0.0";
 
-    JLabel countLabel = new JLabel("0");
+    JLabel runningCountLabel = new JLabel("0");
+    JLabel trueCountLabel = new JLabel("0");
+    JLabel totalDecksLabel = new JLabel(Integer.toString(totalDecks));
+
+    Font mainFont = new Font("Arial", Font.BOLD, 25);
     Color backgroundColor = new Color(40, 40, 40);
-    Color labelColor = new Color(251, 191, 53);
-    // 250, 189, 47
+    Color textColor = new Color(251, 191, 53);
 
     public static void main(String[] args) {
         Main main = new Main();
-        main.createWindow();
+        main.setUpWindow();
     }
 
-    void createWindow() {
-        // window frame set up
+    void setUpWindow() {
+        // set up window frame
         JFrame window = new JFrame();
-        window.setVisible(true);
-        window.setSize(250, 150);
+        window.setSize(300, 200);
         window.setTitle(windowTitle);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.addKeyListener(this);
         window.getContentPane().setBackground(backgroundColor);
         window.setLocationRelativeTo(null);
 
-        // count label set up
-        countLabel.setHorizontalAlignment(JLabel.CENTER);
-        countLabel.setVerticalAlignment(JLabel.CENTER);
-        countLabel.setFont(new Font("Arial", Font.BOLD, 60));
-        countLabel.setForeground(labelColor);
+        // set up main panel
+        JPanel mainPanel = new JPanel();
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(10, 30, 15, 30));
+        mainPanel.setLayout(new GridLayout(0, 1));
+        mainPanel.setBackground(backgroundColor);
 
-        // add label to frame
-        window.add(countLabel);
+        // set up running count panel
+        JPanel runningCountPanel = new JPanel();
+        runningCountPanel.setBackground(backgroundColor);
+
+        JLabel runningText = new JLabel("Run:");
+        runningText.setFont(mainFont);
+        runningText.setForeground(textColor);
+
+        runningCountLabel.setFont(mainFont);
+        runningCountLabel.setForeground(new Color(240,240,240));
+
+        runningCountPanel.add(runningText);
+        runningCountPanel.add(runningCountLabel);
+
+        // set up true count panel
+        JPanel trueCountPanel = new JPanel();
+        trueCountPanel.setBackground(backgroundColor);
+
+        JLabel trueText = new JLabel("True:");
+        trueText.setFont(mainFont);
+        trueText.setForeground(textColor);
+
+        trueCountLabel.setFont(mainFont);
+        trueCountLabel.setForeground(new Color(221, 160, 221));
+
+        trueCountPanel.add(trueText);
+        trueCountPanel.add(trueCountLabel);
+
+        // set up total decks panel
+        JPanel totalDecksPanel = new JPanel();
+        totalDecksPanel.setBackground(backgroundColor);
+
+        JLabel totalText = new JLabel("Total decks:");
+        totalText.setForeground(new Color(153, 153, 153));
+
+        JButton totalDecksEdit = new JButton("Edit");
+        totalDecksEdit.setFocusable(false);
+        totalDecksEdit.addActionListener(this);
+
+        totalDecksLabel.setForeground(new Color(240, 240, 240));
+        totalDecksLabel.setFont(new Font("Arial", Font.BOLD, 22));
+
+        totalDecksPanel.add(totalText);
+        totalDecksPanel.add(totalDecksLabel);
+        totalDecksPanel.add(totalDecksEdit);
+
+        // add count panels to main panel
+        mainPanel.add(runningCountPanel);
+        mainPanel.add(trueCountPanel);
+        mainPanel.add(totalDecksPanel);
+
+        // add main panel to window
+        window.add(mainPanel, BorderLayout.WEST);
+        window.pack();
+        window.setVisible(true);
     }
 
-    void updateCountLabel() {
-        countLabel.setText(Integer.toString(count));
+    void updateRunningCountLabel() {
+        runningCountLabel.setText(Integer.toString(runningCount));
+    }
+
+    void updateTrueCountLabel() {
+        trueCount = runningCount / totalDecks;
+        trueCountLabel.setText(Integer.toString(trueCount));
+    }
+
+    void updateTotalDecksLabel() {
+        totalDecksLabel.setText(Integer.toString(totalDecks));
     }
 
     @Override
@@ -51,20 +119,46 @@ public class Main implements KeyListener {
     public void keyPressed(KeyEvent e) {
         switch(e.getKeyCode()) {
             case 37:
-                count--;
+                runningCount--;
                 break;
             case 39:
-                count++;
+                runningCount++;
                 break;
             case 82:
-                count = 0;
+                runningCount = 0;
                 break;
         }
-        updateCountLabel();
+
+        updateRunningCountLabel();
+        updateTrueCountLabel();
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
 
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        displayNumDecksDialog();
+    }
+
+    void displayNumDecksDialog() {
+        String decks = JOptionPane.showInputDialog("Number of decks:", totalDecks);
+
+        if (decks != null && isInteger(decks)) {
+            totalDecks = Integer.parseInt(decks);
+            updateTotalDecksLabel();
+            updateTrueCountLabel();
+        }
+    }
+
+    boolean isInteger(String s) {
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch(NumberFormatException e) {
+            return false;
+        }
     }
 }
